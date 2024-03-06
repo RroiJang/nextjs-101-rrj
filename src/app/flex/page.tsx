b9c95523427b6b793d7078077f4a8a7c64d326cd
pongsagornchumnankul <pongsagorn24@hotmail.com>
2024-03-06 17:13:08 +0700

    RRJ

--- src/app/flex/page.tsx
+++ src/app/flex/page.tsx
@@ -13,47 +13,18 @@ export default function Flex() {
 
   return (
 
-    <div className="flex justify-center items-center w-full h-screen bg-gray-200">
-      <div className="flex flex-col shadow-2xl justify-evenly rounded-2xl h-fit w-[450px] h-[620px] bg-white">
-
-        <div className="flex h-[100%] justify-center items-center" >
-          <div className="flex justify-center items-center flex-col gap-4">
-            <h1 className="font-extrabolt text-3xl">WELLCOME R.ROI.JANG</h1>
-            <Image className="hover:w-24 hover:h-24" src="next.svg" alt="" width={100} height={100} />
-          </div>
-        </div>
-
-
-        <div className="h-[100%] border-2 border-green-700">
-          <TextField className="w-[270px]" id="standard-basic" label="Email" variant="standard" />
-          <TextField
-            className="w-[270px]"
-            id="standard-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="standard" />
-        </div>
-
-
-        <div className="h-[100%] border-2 border-green-700">
-          fgfghfghfgh
-        </div>
-
-      </div>
-    </div>
-
     // <div className="flex justify-center items-center w-full h-screen bg-gray-200">
-    //   <div className="flex flex-col justify-evenly rounded-xl w-[450px] h-[620px] bg-white shadow-md">
+    //   <div className="flex flex-col shadow-2xl justify-evenly rounded-2xl h-fit w-[450px] h-[620px] bg-white">
 
-    //     <div className="flex justify-center items-center h-[100%]">
-    //       <div className="flex justify-center items-center flex-col gap-8">
-    //         <h1 className="font-extrabold text-3xl">Welcome Best</h1>
-    //         <Image className="hover:w-24 hover:h-24" alt="" src="next.svg" width={70} height={70} />
+    //     <div className="flex h-[100%] justify-center items-center" >
+    //       <div className="flex justify-center items-center flex-col gap-4">
+    //         <h1 className="font-extrabolt text-3xl">WELLCOME R.ROI.JANG</h1>
+    //         <Image className="hover:w-24 hover:h-24" src="next.svg" alt="" width={100} height={100} />
     //       </div>
     //     </div>
 
-    //     <div className="h-[100%] flex flex-col">
+
+    //     <div className="h-[100%] border-2 border-green-700">
     //       <TextField className="w-[270px]" id="standard-basic" label="Email" variant="standard" />
     //       <TextField
     //         className="w-[270px]"
@@ -61,13 +32,42 @@ export default function Flex() {
     //         label="Password"
     //         type="password"
     //         autoComplete="current-password"
-    //         variant="standard"
-    //       />
+    //         variant="standard" />
     //     </div>
 
-    //     <div className="h-[100%] border-2 border-blue-500"></div>
+
+    //     <div className="h-[100%] border-2 border-green-700">
+    //       fgfghfghfgh
+    //     </div>
+
     //   </div>
     // </div>
+
+    <div className="flex justify-center items-center w-full h-screen bg-gray-200">
+      <div className="flex flex-col justify-evenly rounded-xl w-[450px] h-[620px] bg-white shadow-md">
+
+        <div className="flex justify-center items-center h-[100%]">
+          <div className="flex justify-center items-center flex-col gap-8">
+            <h1 className="font-extrabold text-3xl">Welcome Best</h1>
+            <Image className="hover:w-24 hover:h-24" alt="" src="next.svg" width={70} height={70} />
+          </div>
+        </div>
+
+        <div className="h-[100%] flex flex-col">
+          <TextField className="w-[270px]" id="standard-basic" label="Email" variant="standard" />
+          <TextField
+            className="w-[270px]"
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+          />
+        </div>
+
+        <div className="h-[100%] border-2 border-blue-500"></div>
+      </div>
+    </div>
   )
 }
 
